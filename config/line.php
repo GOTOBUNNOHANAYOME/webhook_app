1360a04da1dacf0c4e79e91f5f2f719dbd7b7c21
test <test@Kentas-MacBook-Pro.local>
2024-07-31 23:29:48 +0900

    LINEの環境変数の追加

--- config/line.php
+++ config/line.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'default' => env('LINE_ENDPOINT'),
-    'data'    => env('LINE_DATA_ENDPOINT')
+    'default'      => env('LINE_ENDPOINT'),
+    'data'         => env('LINE_DATA_ENDPOINT'),
+    'access_token' => env('access_token'),
+    'message'      => env('LINE_MESSAGE_URL'),
 ];
