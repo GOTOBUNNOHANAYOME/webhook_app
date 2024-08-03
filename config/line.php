1957acc6829cb761ce39a0e63f04d2cf1e2da52b
test <test@Kentas-MacBook-Pro.local>
2024-08-04 00:51:01 +0900

    configの追加

--- config/line.php
+++ config/line.php
@@ -5,4 +5,5 @@ return [
     'data'         => env('LINE_DATA_ENDPOINT'),
     'access_token' => env('access_token'),
     'message'      => env('LINE_MESSAGE_URL'),
+    'get_profile'  => 'https://api.line.me/v2/bot/profile',
 ];
