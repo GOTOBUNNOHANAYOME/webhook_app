bd28367c692b40bdfad5faa78406ef18229dc841
test <test@Kentas-MacBook-Pro.local>
2024-08-03 17:12:44 +0900

    ログイン用ミドルウェアを追加

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\LoginHistoryController;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return to_route('login_histories.create');
 });
 
-Route::controller(LoginHistoryController::class)->prefix('login_histories')->as('login_histories.')->group(function(){
-    Route::get('/create', 'create')->name('create');
-    Route::post('/store', 'store')->name('store');
+Route::middleware(['login.verify'])->group(function(){    
+    Route::controller(LoginHistoryController::class)->prefix('login_histories')->as('login_histories.')->group(function(){
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+    });
 });
