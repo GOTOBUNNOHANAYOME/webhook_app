9931493bd053b1be72a1e35fc6491e8c12e52dd4
test <test@Kentas-MacBook-Pro.local>
2024-08-03 16:40:04 +0900

    routeの編集

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
 use App\Http\Controllers\LoginHistoryController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return to_route('login_histories.create');
-});
-
-Route::controller(UserController::class)->prefix('users')->as('users.')->group(function(){
-
 });
 
 Route::controller(LoginHistoryController::class)->prefix('login_histories')->as('login_histories.')->group(function(){
