cdaed27f05f38c3e3727d3fdf8c4aea4e1d93b4f
test <test@Kentas-MacBook-Pro.local>
2024-08-04 00:50:23 +0900

    マイグレーション修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('line_user_id')->unique();
+            $table->string('email')->nullable()->unique();
+            $table->string('password')->nullable();
             $table->integer('is_enable');
             $table->rememberToken();
             $table->timestamps();
