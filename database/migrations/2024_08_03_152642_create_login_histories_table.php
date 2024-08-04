bc4c1c7000bd026adbf193814c9069c5617b9520
test <test@Kentas-MacBook-Pro.local>
2024-08-04 14:10:13 +0900

    マイグレーションの構文エラーの修正

--- database/migrations/2024_08_03_152642_create_login_histories_table.php
+++ database/migrations/2024_08_03_152642_create_login_histories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('login_histories', function (Blueprint $table) {
+        Schema::create('login_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('ip');
@@ -25,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('login_histories', function (Blueprint $table) {
-            Schema::dropIfExists('login_histories');
-        });
+        Schema::dropIfExists('login_histories');
     }
 };
