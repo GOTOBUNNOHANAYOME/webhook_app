10da5924373f2a05b2fbf86fdcfe7748fe3e98c2
test <test@Kentas-MacBook-Pro.local>
2024-08-04 16:32:25 +0900

    環境変数の呼び出し

--- config/line.php
+++ config/line.php
@@ -3,7 +3,7 @@
 return [
     'default'      => env('LINE_ENDPOINT'),
     'data'         => env('LINE_DATA_ENDPOINT'),
-    'access_token' => env('access_token'),
+    'access_token' => env('LINE_ACCESS_TOKEN'),
     'message'      => env('LINE_MESSAGE_URL'),
     'get_profile'  => 'https://api.line.me/v2/bot/profile',
 ];
